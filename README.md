1987e58ac707d48ad2f8857e53d34e513a63cf07
Kumar Shraiyash <justshraiyash@gmail.com>
2021-03-22 04:09:08 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# you are confused not your Food
+are you confused what to order/cook everyday. Have a set of food items for breakfast/lunch/dinner.
---
+++ src/Menu.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, img, desc, fav } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                {fav && <h5 className="fav">preferred*</h5>}
+              </header>
+              <p className="item-text">{desc}</p>
+              <h6><a href="https://www.youtube.com/" >Watch now</a></h6>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
